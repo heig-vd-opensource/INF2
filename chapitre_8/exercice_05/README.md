2260ceb14865f40616a86b72bde542cd409bae31
heig-vd-opensource <heig-vd-opensource@protonmail.com>
2018-04-08 16:31:41 +0200

    chap8 -ex05

--- chapitre_8/exercice_05/README.md
+++ chapitre_8/exercice_05/README.md
@@ -1,1 +1,22 @@
-#Exercice 5
+# Exercice 5
+## Instanciations d'une classe générique
+
+Soient les deux classes génériques (ou patrons de classes) suivantes :
+
+```cpp
+template <typename T = int> class A {…};
+template <typename T, typename U, int n> class B {…};
+```
+
+Pour chacune des instanciations ci-dessous, dire si celle-ci est correcte ou non.
+
+1) `B<int, double, 1> b;`
+2) `const int N = 1; B<int, int, N> b;`
+3) `int n = 1; B<int, double, n> b;`
+4) `B<int, double, 'A'> b;`
+5) `B<int, double, 'A' + 'B'> b;`
+6) `B<int, double, 1.0> b;`
+7) `B<int, int*, 1> b;`
+8) `B<A<>, double, 1> b;`
+9) `B<double, A, 1> b;`
+10) `B<A<double>, double, true> b;`
---
+++ chapitre_8/exercice_05/main.cpp
@@ -0,0 +1,55 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * patrons de classes:
+ */
+template <typename T = int> class A {
+
+};
+
+template <typename T, typename U, int n> class B {
+
+};
+
+
+int main(){
+    // 1)
+    B<int, double, 1> b1;
+
+    // 2)
+    const int N = 1;
+    B<int, int, N> b2;
+
+    // 3)
+    B<int, double, 'A'> b3;
+
+    // 4)
+    B<int, double, 'A'> b4;
+
+    // 5)
+    B<int, double, 'A' + 'B'> b5;
+
+    // 6) incorrect
+    // error: conversion from ‘double’ to ‘int’ not considered for non-type template argument
+    // error: could not convert template argument ‘1.0e+0’ to ‘int’
+//    B<int, double, 1.0> b6;
+
+    // 7)
+    B<int, int*, 1> b7;
+
+    // 8)
+    B<A<>, double, 1> b8;
+
+    // 9) incorrect
+    // error: type/value mismatch at argument 2 in template parameter list
+    // for ‘template<class T, class U, int n> class B’
+//    B<double, A, 1> b9;
+
+    // 10)
+    B<A<double>, double, true> b10;
+
+
+}
