a28dcbe80dd4c3aab556f07c1da8256f18fb3474
heig-vd-opensource <heig-vd-opensource@protonmail.com>
2018-04-08 18:29:57 +0000

    chap8 - ex7

--- chapitre_8/exercice_07/main.cpp
+++ chapitre_8/exercice_07/main.cpp
@@ -4,21 +4,29 @@
 
 using namespace std;
 
-
 template <typename T, size_t n> class Array {
 public:
-q     template <typename T, size_t n>
-    friend ostream &operator<<(ostream &os, const Array<T, n> &array);
-public:
     Array(const T& valeur);
+    void afficher()const ;
+    template<typename X, size_t y>
+    friend void abcd(const Array<X, y>& array);
+    template<typename Q, size_t r>
+    friend ostream &operator << (ostream& os, const Array<Q, r>& array);
 private:
     T tab[n];
 };
 
 template <typename T, size_t n>
-ostream &operator<<(ostream &os, const Array<T, n> &array) {
-    os << "tab: " << array.tab;
+ostream &operator <<(ostream &os, const Array<T, n> &array){
+    for(size_t i=0; i < n; ++i){
+        os << array.tab[i];
+    }
     return os;
+}
+
+template <typename T, size_t n>
+void abcd(const Array<T, n> &array){
+    cout << "ici" << endl;
 }
 
 template<typename T, size_t n>
@@ -28,12 +36,27 @@ Array<T, n>::Array(const T &valeur) {
     }
 }
 
+template<typename T, size_t n>
+void Array<T, n>::afficher() const {
+    cout << "[";
+    for(size_t i=0; i<n; ++i) {
+        if(i > 0){
+            cout << ", ";
+        }
+        cout << tab[i];
+    }
+    cout << "]";
+}
+
+
 int main() {
     Array<int, 3> a1(1);
-    cout << a1 << endl;
+    cout << a1;
+    cout << endl;
 
     Array<double, 4> a2(1.5);
-    cout << a2 << endl;
+    cout << a2;
+    cout << endl;
 
     return EXIT_SUCCESS;
 }
